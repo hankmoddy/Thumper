6b3609eebaf79702451ee968afaeb1735fd70d89
Stéphane Goetz <onigoetz@onigoetz.ch>
2013-05-08 13:55:16 +0200

    Updated BWToolkit to run on Xcode 4.3 and up

--- BWToolkit.ibplugin/Contents/Frameworks/BWToolkitFramework.framework/Versions/A/Headers/BWAnchoredButtonBar.h
+++ BWToolkit.ibplugin/Contents/Frameworks/BWToolkitFramework.framework/Versions/A/Headers/BWAnchoredButtonBar.h
@@ -5,7 +5,7 @@
 //  Created by Brandon Walkin (www.brandonwalkin.com)
 //  All code is provided under the New BSD license.
 //
-
+//
 #import <Cocoa/Cocoa.h>
 
 @interface BWAnchoredButtonBar : NSView 
